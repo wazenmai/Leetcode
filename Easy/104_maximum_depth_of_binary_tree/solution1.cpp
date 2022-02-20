b69a03dcd8b9622afbf9d2d3bc9cfcd717789031
wazenmai <iotbwu0101@gmail.com>
2022-02-20 14:32:06 +0800

    February 14 Challenge

---
+++ Easy/104_maximum_depth_of_binary_tree/solution1.cpp
@@ -0,0 +1,25 @@
+/**
+ * Title:  Maximum Depth of Binary Tree (Leetcode Easy)
+ * Author: Bronwin Chen <iotbwu0101@gmail.com>
+ * Date:   14, Feb, 2022
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
