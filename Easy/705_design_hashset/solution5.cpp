eb3c2dbc98fbcfd9d88e62c581431048999c271d
wazenmai <iotbwu0101@gmail.com>
2022-04-21 11:15:03 +0200

    2022 April 21 challenge: improve space complexity

--- Easy/705_design_hashset/solution5.cpp
+++ Easy/705_design_hashset/solution5.cpp
@@ -3,31 +3,31 @@
  * Title:  Design Hashset (Leetcode Easy)
  * Author: Bronwin Chen <iotbwu0101@gmail.com>
  * Date:   21, April, 2022
- * Method: Use bit-manipulation (unsigned long) to construct hash set, O(1) time each operation. 
+ * Method: Use bit-manipulation (char) to construct hash set, O(1) time each operation. 
  */
 
 class MyHashSet {
 private:
-    vector<unsigned long> v;
+    vector<char> v;
 public:
-    MyHashSet(): v(31251, 0) {}
+    MyHashSet(): v(125001, 0) {}
     
     void add(int key) {
-        unsigned long pos = key / 32;
-        unsigned long bit = key % 32;
+        int pos = key / 8;
+        int bit = key % 8;
         v[pos] |= (1 << bit);
     }
     
     void remove(int key) {
-        unsigned long pos = key / 32;
-        unsigned long bit = key % 32;
-        unsigned long num = (0xffffffff ^ (1 << bit));
+        int pos = key / 8;
+        int bit = key % 8;
+        int num = (0xff ^ (1 << bit));
         v[pos] &= num;
     }
     
     bool contains(int key) {
-        unsigned long pos = key / 32;
-        unsigned long bit = key % 32;
+        int pos = key / 8;
+        int bit = key % 8;
         return (v[pos] & (1 << bit));
     }
 };
