1bbbebeb333722366f7221a9193c6dbf38f82f31
wazenmai <iotbwu0101@gmail.com>
2024-11-03 11:40:09 +0800

    2024 November 03 Daily Challenge

---
+++ Easy/796_rotate_string/solution1.cpp
@@ -0,0 +1,33 @@
+/**
+ * Title:  Rotate String (Leetcode Easy 796)
+ * Author: Bronwin Chen <iotbwu0101@gmail.com>
+ * Date:   03, November, 2024
+ */
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        int n = s.length();
+        if (n != goal.length()) return false;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == goal[0]) {
+                bool flag = true;
+                for (int j = i; j < n; j++) {
+                    if (s[j] != goal[j - i]) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (!flag) continue;
+                for (int j = 0; j < i; j++) {
+                    if (s[j] != goal[n - i + j]) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) return true;
+            }
+        }
+        return false;
+    }
+};
